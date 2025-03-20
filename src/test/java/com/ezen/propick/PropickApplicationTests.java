49cc4d5472c0eafad733b593d38590bdd438bbf5
hwangjaeyong <beomgolaeda@gmail.com>
2025-03-20 12:42:35 +0900

    기본 구조

---
+++ src/test/java/com/ezen/propick/PropickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezen.propick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
