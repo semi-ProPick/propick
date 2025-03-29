ec5b948435c81a2c128fc2c2ea907c67e03dce91
hwangjaeyong <beomgolaeda@gmail.com>
2025-03-29 13:42:38 +0900

    3월 29일 아이디 찾기 완료

---
+++ src/main/java/com/ezen/propick/user/dto/findIdDTO.java
@@ -0,0 +1,13 @@
+package com.ezen.propick.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class findIdDTO {
+    private String userId;
+}
