ae58a5d9f14dc51ca7098311206354e79c141efd
hwangjaeyong <beomgolaeda@gmail.com>
2025-04-07 15:20:19 +0900

    4/7 유저, 게시판, 상품 머지

--- src/main/java/com/ezen/propick/admin/controller/NoticeController.java
+++ src/main/java/com/ezen/propick/admin/controller/NoticeController.java
@@ -1,11 +1,9 @@
-package com.ezen.propick.board.controller;
+package com.ezen.propick.admin.controller;
 
-import com.ezen.propick.board.entity.FnaBoard;
 import com.ezen.propick.board.entity.Notice;
-import com.ezen.propick.board.service.FnaBoardService;
 import com.ezen.propick.board.service.NoticeService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,42 +16,43 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Profile("admin")
 @Controller
-public class FnaController {
-    @Autowired
-    private final FnaBoardService fnaBoardService;
+public class NoticeController {
 
-    public FnaController(FnaBoardService fnaBoardService) {
-        this.fnaBoardService = fnaBoardService;
+    @Autowired
+    private final NoticeService noticeService;
+
+    public NoticeController(NoticeService noticeService) {
+        this.noticeService = noticeService;
     }
 
-    @GetMapping("/fna/write")
+    @GetMapping("/notice/write")
     public String noticeWriteForm() {
 
-        return "/management/post_fna_write";
+        return "/management/post_notice_write";
     }
 
-    @PostMapping("/fna/writedo")
-    public String fnaWritePro(FnaBoard fnaBoard) {
-        fnaBoardService.fnawrite(fnaBoard);
-        return "redirect:/fna/list";
+    @PostMapping("/notice/writedo")
+    public String noticeWritePro(Notice notice) {
+        noticeService.noticewrite(notice);
+        return "redirect:/notice/list";
 
     }
-
-    @GetMapping("/fna/list")
-    public String fnalist(Model model,
+    @GetMapping("/notice/list")
+    public String noticelist(Model model,
                              @PageableDefault(page=0, size=10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
                              String searchKeyword,
                              @RequestParam(value = "page", defaultValue = "0") int page) { //url 에서 ?page=1, ?page=0등의 값 받아옴
 
-        Page<FnaBoard> list = null;
+        Page<Notice> list = null;
         //searchKeyword 가 넣이면 원래 페이지 리스트 보여줌
         if(searchKeyword == null) {
-            list = fnaBoardService.fnaBoardList(pageable);
+            list = noticeService.noticeList(pageable);
 
             //searchKeyword 가 널이 아니면 작성한 검색 메서드 실행 후 페이지 리스트 보여줌
         } else {
-            list = fnaBoardService.fnaSearchList(searchKeyword, pageable);
+            list = noticeService.noticeSearchList(searchKeyword, pageable);
         }
         // 현재 페이지를 기반으로 pageable 생성
         pageable = PageRequest.of(page, pageable.getPageSize(), pageable.getSort());
@@ -70,54 +69,42 @@ public class FnaController {
         model.addAttribute("nowPage", nowPage);
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
-        return "/management/post_fna";
+        return "/management/post_notice";
     }
 
-    @GetMapping("/fna/view")   //localhost:8081/notice/view?id=1
-    public String fnaView(Model model, Integer id){
-        model.addAttribute("fnaBoard", fnaBoardService.fnaView(id));
-        return "/management/post_fna_view";
+    @GetMapping("/notice/view")   //localhost:8081/notice/view?id=1
+    public String noticeView(Model model, Integer id){
+        model.addAttribute("notice", noticeService.noticeView(id));
+        return "/management/post_notice_view";
     }
 
-    @GetMapping("fna/delete")
-    public String fnaDelete(Integer id){
-        fnaBoardService.fnaDelete(id);
+    @GetMapping("notice/delete")
+    public String noticeDelete(Integer id){
+        noticeService.noticeDelete(id);
         return "redirect:list";
     }
 
     //수정 @PathVariable 사용
-    @GetMapping("fna/modify/{id}")
-    public String fnaModify(@PathVariable("id") Integer id, Model model){
+    @GetMapping("notice/modify/{id}")
+    public String noticeModify(@PathVariable("id") Integer id, Model model){
 
-        model.addAttribute("fnaBoard", fnaBoardService.fnaView(id));
-        return "/management/post_fna_modify";
+        model.addAttribute("notice", noticeService.noticeView(id));
+        return "/management/post_notice_modify";
     }
 
     //수정
-    @PostMapping("/fna/update/{id}")
-    public String fnaUpdate(@PathVariable("id") Integer id, FnaBoard fnaBoard) {
+    @PostMapping("/notice/update/{id}")
+    public String noticeUpdate(@PathVariable("id") Integer id, Notice notice) {
 
         //기존 내용 가져옴
-        FnaBoard boardTemp = fnaBoardService.fnaView(id);
+        Notice boardTemp = noticeService.noticeView(id);
         //위의 인수로 받은 새로운 내용 가져오기-> 덮어씌우기
-        boardTemp.setTitle(fnaBoard.getTitle());
-        boardTemp.setAnswer(fnaBoard.getAnswer());
+        boardTemp.setTitle(notice.getTitle());
+        boardTemp.setContents(notice.getContents());
 
-        fnaBoardService.fnawrite(boardTemp);
+        noticeService.noticewrite(boardTemp);
 
-        return "redirect:/fna/list";
+        return "redirect:/notice/list";
     }
 
-
-//    //게시글 리스트 출력
-//    @GetMapping("/main/f&q_board")
-//    public String fnaBoardList(Model model) {
-//        model.addAttribute("list", fnaBoardService.fnaBoardList());
-//
-//        return "/main/f&q_board";
-//    }
-
-
-
 }
-
--- src/main/java/com/ezen/propick/main/MainController.java
+++ src/main/java/com/ezen/propick/main/MainController.java
@@ -1,8 +1,10 @@
 package com.ezen.propick.main;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+@Profile("user")
 @Controller
 public class MainController {
     @GetMapping("/")
