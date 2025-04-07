84823c229c4418f3a3053bf0cd23293fc4b2bceb
yein <ehowldpdls@gmail.com>
2025-04-07 09:48:47 +0900

    templete 추가

---
+++ src/main/java/com/ezen/propick/main/MainController.java
@@ -0,0 +1,12 @@
+package com.ezen.propick.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainPage() {
+        return "main/main";
+    }
+}
