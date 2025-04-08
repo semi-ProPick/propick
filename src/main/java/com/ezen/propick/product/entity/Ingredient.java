4e469cf9c239492deb603f67ec76106b6083d351
hwangjaeyong <beomgolaeda@gmail.com>
2025-04-08 12:09:03 +0900

    제품 누르면 상세페이지

--- src/main/java/com/ezen/propick/product/entity/Ingredient.java
+++ src/main/java/com/ezen/propick/product/entity/Ingredient.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "ingredients")
+@Table(name = "ingredient")
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
